2f815fe899738accbb2deb760c462cc36715a879
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-11-08 23:36:01 -0300

    test

---
+++ src/main/java/com/facens/devopsproject/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.facens.devopsproject.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+
+}
